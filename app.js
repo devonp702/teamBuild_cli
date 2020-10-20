d24796075d469173c3b5fc01b4c8393bba261fe9
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-10-20 16:41:59 -0600

    fixed the fs writing errors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
-const questions1 = [
+const questions = [
   {
     type: "input",
     name:"mName",
@@ -45,26 +45,27 @@ const questions1 = [
   }
 ]
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer.prompt(questions1)
+if(!fs.existsSync(OUTPUT_DIR)){
+  fs.mkdirSync(OUTPUT_DIR)
+}
+inquirer.prompt(questions)
 .then(data => {
 let me = new Manager(data.mName, data.mId, data.mEmail, data.mOn)
-let employees = [me];
-let html = render(employees);
-fs.writeFile(outputPath, html, function(err){
-  if (err) throw err;
-  console.log("file saved")
-});
-});
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
+let employees = [me];
+let html = render(employees);
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+fs.writeFile(outputPath, html, function(err){
+  if (err) throw err;
+  console.log("file saved")
+});
+});
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
